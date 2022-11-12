36605aed77b980c0f54b0f07401959168a81474b
kimberlyl3 <105944389+kimliu3@users.noreply.github.com>
2022-11-12 18:42:31 -0500

    everything

---
+++ Assets/Health.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    [SerializeField] private int health = 50;
+    private int MAX_HEALTH = 50;
+
+    private void Start() {
+        health = MAX_HEALTH;
+    }
+
+    public void Damage(int amount) {
+        this.health -= amount;
+        Debug.Log(this.health);
+
+        if (this.health <= 0) {
+            Die();
+            Destroy(gameObject);
+        }
+    }
+
+    public void Heal(int amount) {
+        if (this.health + amount > MAX_HEALTH) 
+        {
+            this.health = MAX_HEALTH;
+        } else {
+            this.health += amount;
+        }
+    }
+
+    private void Die() {
+        RandomSpawner.enemiesAlive--;
+    }
+}
---
+++ Assets/RandomSpawner.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform[] spawnPoints;
+    public GameObject enemy;
+    public float spawnTimer = 0f;
+    public float gameTimer = 0f;
+    public float spawnTime = 5f;
+    public static int enemiesAlive = 0;
+    public int maxEnemies = 8;
+
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // update timers
+        spawnTimer += Time.deltaTime;
+        gameTimer += Time.deltaTime;
+
+        // if spawntime is reached, increase time
+        if (spawnTimer >= spawnTime && enemiesAlive < maxEnemies){
+            int randPoint = Random.Range(0, spawnPoints.Length);
+            Instantiate(enemy, spawnPoints[randPoint].position, transform.rotation);
+            spawnTimer = 0f;
+            enemiesAlive++;
+        }
+
+        if (gameTimer % 15 == 0) {
+            spawnTime -= 0.1f;
+        }
+        
+    }
+}
