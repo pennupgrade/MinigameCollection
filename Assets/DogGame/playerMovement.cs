f40c9ca09a19469551bfa8fdb8be79c6a41f9e54
philipch07 <philipch07@gmail.com>
2022-12-03 17:45:44 -0500

    poopy

--- Assets/DogGame/playerMovement.cs
+++ Assets/DogGame/playerMovement.cs
@@ -33,11 +33,10 @@ public class playerMovement : MonoBehaviour
         //}
 
         if (mx > 0f) {
-            transform.localScale = new Vector3(1f, 1f, 1f);
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
             isFacingRight = true;
-        }
-        else if (mx < 0f) {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
+        } else if (mx < 0f) {
+            transform.localScale = new Vector3(-1 * Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
             isFacingRight = false;
         }
 
