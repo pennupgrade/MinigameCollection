276129f19cada3442b8587e4e12451193407033b
zminsc <zawmin.stevenchang@gmail.com>
2023-01-28 17:22:12 -0500

    Implemented player movement

---
+++ Assets/Scripts/RiverMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiverMovement : MonoBehaviour
+{
+    private float movementSpd = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.position += new Vector3(0, movementSpd * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.position += new Vector3(-movementSpd * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.position += new Vector3(0, -movementSpd * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.position += new Vector3(movementSpd * Time.deltaTime, 0, 0);
+        }
+    }
+}
