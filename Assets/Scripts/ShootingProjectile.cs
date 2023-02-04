4176123e2b3916f2149cc7bc1183cdc63d9221ea
Erik Wei <erikwei@sas.upenn.edu>
2023-02-04 18:09:06 -0500

    added shooting projectile
    added shooting projectile

---
+++ Assets/Scripts/ShootingProjectile.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingProjectile : MonoBehaviour
+{
+    public float projectileSpeed;
+    public float lifeTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestroyProjectile", lifeTime);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.right * projectileSpeed * Time.deltaTime);
+    }
+
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+}
