512261d2a0a70857eba1e62dc7798e385e85f770
Steven Chang <zminsc@seas.upenn.edu>
2023-03-14 18:14:49 -0400

    Create wave counter

---
+++ Assets/Scripts/WaveCounter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveCounter : MonoBehaviour
+{
+    int curWave = 0;
+    Text counterText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        counterText = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counterText.text = "Wave " + curWave;
+    }
+
+    public void Increment()
+    {
+        curWave++;
+    }
+}
